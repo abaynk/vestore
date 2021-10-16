5b905379ac75cd9e0e3393a5a7a0e75b0f12dd04
Abay Nurpeissov <abaynk123@gmail.com>
2021-10-16 06:07:39 +0200

    oformit page

--- src/pages/oformit/OformitPage.js
+++ src/pages/oformit/OformitPage.js
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import "./OformitPage.css";
 import InputMask from "react-input-mask";
 const OformitPage = () => {
-  const [value, setValue] = useState("");
-
+  const [phone, setPhone] = useState("");
+  const [iin, setIin] = useState();
   return (
     <div className="credit_root">
-      <h1>Shas oformim</h1>
       <form>
+        <lable className="label">телефон</lable>
         <InputMask
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
           mask="+7\(999) 999 99 99"
           maskChar="_"
           placeholder="+7(___) ___ __ __"
+          className="phone_input"
+        />
+        <lable className="label">ИИН</lable>
+        <input
+          type="number"
+          placeholder="900101123456"
+          value={iin}
+          onChange={(e) => setIin(e.target.value)}
           className="phone_input"
         />
       </form>
